b4863e5211cdd0c3275366444a77022c5f85e0e8
Reycel Huffman <reycel.huffman@outlook.com>
2022-05-05 18:23:43 -0500

    seeds working

--- models/index.js
+++ models/index.js
@@ -5,12 +5,12 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsToMany(Category, {
+Product.belongsTo(Category, {
 
   foreignKey: 'category_id'
 });
 // Categories have many Products
-Category.belongsToMany(Product, {
+Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
